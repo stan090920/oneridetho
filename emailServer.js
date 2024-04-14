e967ea71d7b24d95dc1029500df47a58cdaaafe5
Sydney <64653592+sydney06@users.noreply.github.com>
2024-04-14 11:48:26 +0300

    Try Force build

--- emailServer.js
+++ emailServer.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
+
 app.use(cors());
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb" }));
